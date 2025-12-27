712c748ce04e986042ee10c0124acf47388dd3d9
LNhart <lukastilmann97@gmail.com>
2025-12-27 23:30:52 +0100

    Fix for fbref blocking requests resulting in 403 Forbidden.

--- R/internals.R
+++ R/internals.R
@@ -436,7 +436,13 @@
 .load_page <- function(page_url) {
   agent <- getOption("worldfootballR.agent", default = "RStudio Desktop (2022.7.1.554); R (4.1.1 x86_64-w64-mingw32 x86_64 mingw32)")
   ua <- httr::user_agent(agent)
-  session <- rvest::session(url = page_url, ua)
+  
+  # Add Sec-CH-UA header to avoid 403 Forbidden
+  custom_headers <- httr::add_headers(
+    `Sec-CH-UA` = '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"'
+  )
+  
+  session <- rvest::session(url = page_url, ua, custom_headers)
   xml2::read_html(session)
 }
 
